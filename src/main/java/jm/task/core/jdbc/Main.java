b1825ffba4ef3e0694f62a7adb8dc80ce8627cf0
max <sharg97@gmail.com>
2022-01-07 11:23:44 +0300

    2.1.1.1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -9,12 +9,12 @@ public class Main {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Will", "Smith", (byte) 50);
-//        userService.saveUser("Rassel", "Crow", (byte) 90);
-//        userService.saveUser("Vova", "Ivanov", (byte) 10);
-//        userService.saveUser("Walter", "White", (byte) 45);
-//        userService.removeUserById(3);
-//        userService.getAllUsers();
-//        userService.cleanUsersTable();
-       // userService.dropUsersTable();
+        userService.saveUser("Rassel", "Crow", (byte) 90);
+        userService.saveUser("Vova", "Ivanov", (byte) 10);
+        userService.saveUser("Walter", "White", (byte) 45);
+        userService.removeUserById(3);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
